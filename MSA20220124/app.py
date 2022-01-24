a0458d4d2ac3a362745ef38bcbee94e781497c58
TannyKim <98008421+TannyKim@users.noreply.github.com>
2022-01-24 17:54:02 +0900

    Add files via upload

---
+++ MSA20220124/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import json
+import requests
+import pandas as pd
+
+
+app = Flask(__name__)
+
+@app.route("/") 
+def FlaskLab(): 
+    return "Flask 데이터 응답" 
+
+@app.route("/data1")
+def FlaskData(): 
+    keyValue = r"wtiiIXr9QnvUtWgl%2BVvLU3WpIOX3gJF4Ff9TZ4ZhLx12DzQPEtgxzTa2z%2F3GypeDAlnciIl7zR29ya7bdqtH7A%3D%3D"   
+
+    dataURL = "http://api.odcloud.kr/api/apnmOrg/v1/list?"
+    dataURL += "page=" + str(1) + "&perPage=" + str(10)
+    dataURL += "&cond" + "%5BorgZipaddr%3A%3ALIKE%5D=%EC%9D%80%ED%8F%89%EA%B5%AC"
+    dataURL += "&serviceKey=" + keyValue
+    
+    dataResult = requests.get(dataURL)
+
+    return json.loads(dataResult)
+
+with open("mydata.json", "w", encoding="utf-8") as writeFile:
+    json.dump(dataResult, writeFile, ensure_ascii=False, indent=4)
+
